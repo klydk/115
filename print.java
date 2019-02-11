f5f22831b5f880b6c1a96a4e31dcb5625896c12d
klydk <41453671+klydk@users.noreply.github.com>
2019-02-11 12:50:12 -0800

    Add files via upload

---
+++ print.java
@@ -0,0 +1,19 @@
+/*
+* Write a method named printGPA...
+*/
+
+public static void printGPA() {
+    Scanner console = new Scanner(System.in);
+    System.out.print("Enter a student record: ");
+    String name = console.next();
+    int numScore = console.nextInt();
+    double sum = 0.0;
+    
+    for (int i = 0; i < numScore; i++) {
+        int score = console.nextInt();
+        sum += score;
+    }
+    
+    double average = sum / numScore;
+    System.out.println(name + "'s grade is " + average);
+}
